6db83c2624c2c8d01da06bf259c69acf478c57f6
HeyAshish <its.ashishdev@gmail.com>
2025-05-12 01:42:15 +0530

    refactor: remove unused content and sitemap-related files

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots() {
         userAgent: "*",
       },
     ],
-    sitemap: "https://onurhan.dev/sitemap.xml",
-    host: "https://onurhan.dev",
+    sitemap: "",
+    host: "",
   };
 }
